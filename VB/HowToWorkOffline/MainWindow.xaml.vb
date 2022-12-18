7fbeb0ac1f40fa74f1d7073062d9e91f5dec2536
DevExpressExampleBot <fakeEmail>
2022-12-18 21:35:54 +0400

    Source auto update [skip ci]

--- VB/HowToWorkOffline/MainWindow.xaml.vb
+++ VB/HowToWorkOffline/MainWindow.xaml.vb
@@ -1,42 +1,42 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
-Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 Imports System.IO
 Imports DevExpress.Xpf.Core
 
 Namespace HowToBindToMDB
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-		End Sub
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			pivotGridControl1.SavePivotGridToFile("pivot.dat", True)
-			pivotGridControl1.DataSource = Nothing
-			pivotGridControl1.Fields.Clear()
-		End Sub
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
 
-		Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			If (Not File.Exists("pivot.dat")) Then
-				DXMessageBox.Show("You should save the PivotGrid into a file first")
-				Return
-			End If
-			pivotGridControl1.RestorePivotGridFromFile("pivot.dat")
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.pivotGridControl1.SavePivotGridToFile("pivot.dat", True)
+            Me.pivotGridControl1.DataSource = Nothing
+            Me.pivotGridControl1.Fields.Clear()
+        End Sub
+
+        Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            If Not File.Exists("pivot.dat") Then
+                DXMessageBox.Show("You should save the PivotGrid into a file first")
+                Return
+            End If
+
+            Me.pivotGridControl1.RestorePivotGridFromFile("pivot.dat")
+        End Sub
+    End Class
 End Namespace
